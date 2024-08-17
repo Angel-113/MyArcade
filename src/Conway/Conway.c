efc53ee7577c46426312d59a4a2b0d17a9acf07e
Angel-B113 <angel.ricahalo3@gmail.com>
2024-08-17 17:48:49 -0600

    Toroidal Game Of Life is almost finished.
    Must work on a user selection initial grid

--- src/Conway/Conway.c
+++ src/Conway/Conway.c
@@ -56,8 +56,8 @@ static void UpdateGame ( void ) {
 static void DrawGame ( void ) {
     BeginDrawing();
     ClearBackground(BLACK);
-    for ( short int i = 1; i < rows - 1; i++ )
-        for ( short int j = 1; j < cols - 1; j++ ) {
+    for ( short int i = 0; i < rows; i++ )
+        for ( short int j = 0; j < cols; j++ ) {
             if ( grid[i][j] ) {
                 DrawRectangleRec((Rectangle){j*SQUARE_SIZE, i*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE}, RAYWHITE);
                 DrawRectangleLinesEx((Rectangle){j*SQUARE_SIZE, i*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE}, 1.0f, GRAY);
@@ -97,7 +97,78 @@ static unsigned char CountNeighbors ( short int i, short int j ) {
     alive_neighbors += i != 0 && j != 0 && i < rows && j < cols && grid[i + 1][j] ? 1 : 0;
     alive_neighbors += i != 0 && j != 0 && i < rows && j < cols && grid[i + 1][j + 1] ? 1 : 0;
 
-    /* When i and j are in the matrix borers */
+    /* When i and j are in the matrix borders */
+
+    /* if i = 0 and j = 0 */
+    alive_neighbors += i == 0 && j == 0 && grid[rows - 1][cols - 1] ? 1 : 0;
+    alive_neighbors += i == 0 && j == 0 && grid[rows - 1][cols - 2] ? 1 : 0;
+    alive_neighbors += i == 0 && j == 0 && grid[rows - 1][cols - 3] ? 1 : 0;
+    alive_neighbors += i == 0 && j == 0 && grid[rows - 2][cols - 1] ? 1 : 0;
+    alive_neighbors += i == 0 && j == 0 && grid[rows - 2][cols - 2] ? 1 : 0;
+    alive_neighbors += i == 0 && j == 0 && grid[rows - 2][cols - 3] ? 1 : 0;
+    alive_neighbors += i == 0 && j == 0 && grid[rows - 3][cols - 1] ? 1 : 0;
+    alive_neighbors += i == 0 && j == 0 && grid[rows - 3][cols - 2] ? 1 : 0;
+    alive_neighbors += i == 0 && j == 0 && grid[rows - 3][cols - 3] ? 1 : 0;
+
+    /* if i = rows - 1 and j = cols - 1 */
+    alive_neighbors += i == rows - 1 && j == cols - 1 && grid[0][0] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j == cols - 1 && grid[0][1] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j == cols - 1 && grid[0][2] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j == cols - 1 && grid[1][0] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j == cols - 1 && grid[1][1] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j == cols - 1 && grid[1][2] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j == cols - 1 && grid[2][0] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j == cols - 1 && grid[2][1] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j == cols - 1 && grid[2][2] ? 1 : 0;
+
+    /* if i = 0 or j = 0 */
+
+    /* if i = 0 and j != 0 */
+    alive_neighbors += i == 0 && j != 0 && j != cols - 1 && grid[rows - 1][j - 1] ? 1 : 0;
+    alive_neighbors += i == 0 && j != 0 && j != cols - 1 && grid[rows - 1][j] ? 1 : 0;
+    alive_neighbors += i == 0 && j != 0 && j != cols - 1 && grid[rows - 1][j + 1] ? 1 : 0;
+    alive_neighbors += i == 0 & j != 0 && j != cols - 1 && grid[rows - 2][j - 1] ? 1 : 0;
+    alive_neighbors += i == 0 & j != 0 && j != cols - 1 && grid[rows - 2][j] ? 1 : 0;
+    alive_neighbors += i == 0 & j != 0 && j != cols - 1 && grid[rows - 2][j + 1] ? 1 : 0;
+    alive_neighbors += i == 0 & j != 0 && j != cols - 1 && grid[rows - 3][j - 1] ? 1 : 0;
+    alive_neighbors += i == 0 & j != 0 && j != cols - 1 && grid[rows - 3][j] ? 1 : 0;
+    alive_neighbors += i == 0 & j != 0 && j != cols - 1 && grid[rows - 3][j + 1] ? 1 : 0;
+
+    /* if j = 0 and i != 0 */
+    alive_neighbors += j == 0 && i != 0 && i != rows - 1 && grid[i - 1][cols - 1] ? 1 : 0;
+    alive_neighbors += j == 0 && i != 0 && i != rows - 1 && grid[i - 1][cols - 2] ? 1 : 0;
+    alive_neighbors += j == 0 && i != 0 && i != rows - 1 && grid[i - 1][cols - 3] ? 1 : 0;
+    alive_neighbors += j == 0 && i != 0 && i != rows - 1 && grid[i][cols - 1] ? 1 : 0;
+    alive_neighbors += j == 0 && i != 0 && i != rows - 1 && grid[i][cols - 2] ? 1 : 0;
+    alive_neighbors += j == 0 && i != 0 && i != rows - 1 && grid[i][cols - 3] ? 1 : 0;
+    alive_neighbors += j == 0 && i != 0 && i != rows - 1 && grid[i + 1][cols - 1] ? 1 : 0;
+    alive_neighbors += j == 0 && i != 0 && i != rows - 1 && grid[i + 1][cols - 2] ? 1 : 0;
+    alive_neighbors += j == 0 && i != 0 && i != rows - 1 && grid[i + 1][cols - 3] ? 1 : 0;
+
+
+    /* When i = rows - 1 or j = cols - 1 */
+
+    /* if i = rows - 1 and j != cols - 1 */
+    alive_neighbors += i == rows - 1 && j != 0 && j != cols - 1 && grid[0][j - 1] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j != 0 && j != cols - 1 && grid[0][j] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j != 0 && j != cols - 1 && grid[0][j + 1] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j != 0 && j != cols - 1 && grid[1][j - 1] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j != 0 && j != cols - 1 && grid[1][j] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j != 0 && j != cols - 1 && grid[1][j + 1] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j != 0 && j != cols - 1 && grid[2][j - 1] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j != 0 && j != cols - 1 && grid[2][j] ? 1 : 0;
+    alive_neighbors += i == rows - 1 && j != 0 && j != cols - 1 && grid[2][j + 1] ? 1 : 0;
+
+    /* if j = cols - 1 and i != rows - 1 */
+    alive_neighbors += j == cols - 1 && i != 0 && i != rows - 1 && grid[i - 1][0] ? 1 : 0;
+    alive_neighbors += j == cols - 1 && i != 0 && i != rows - 1 && grid[i - 1][1] ? 1 : 0;
+    alive_neighbors += j == cols - 1 && i != 0 && i != rows - 1 && grid[i - 1][2] ? 1 : 0;
+    alive_neighbors += j == cols - 1 && i != 0 && i != rows - 1 && grid[i][0] ? 1 : 0;
+    alive_neighbors += j == cols - 1 && i != 0 && i != rows - 1 && grid[i][1] ? 1 : 0;
+    alive_neighbors += j == cols - 1 && i != 0 && i != rows - 1 && grid[i][2] ? 1 : 0;
+    alive_neighbors += j == cols - 1 && i != 0 && i != rows - 1 && grid[i + 1][0] ? 1 : 0;
+    alive_neighbors += j == cols - 1 && i != 0 && i != rows - 1 && grid[i + 1][1] ? 1 : 0;
+    alive_neighbors += j == cols - 1 && i != 0 && i != rows - 1 && grid[i + 1][2] ? 1 : 0;
 
     return alive_neighbors;
 }
